e772f78b9d67f47e1e1b225acd104ee1dfb544f9
danieldjupvik <daniel@kravia.ai>
2023-03-26 00:52:19 +0100

    try to fix swagger css

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -2,25 +2,25 @@ const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const path = require('path');
+const mime = require('mime');
 
 const app = express();
 
 const swaggerDocument = YAML.load(path.join(__dirname, './swagger.yaml'));
 
-// Set up Swagger UI using CDN
-const swaggerUiOptions = {
-  customCss:
-    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.18.1/swagger-ui.css',
-  customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.18.1/',
-  customJs:
-    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.18.1/swagger-ui-bundle.js',
-  customJsUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.18.1/',
-};
-app.use(
-  '/swagger',
-  swaggerUi.serve,
-  swaggerUi.setup(swaggerDocument, swaggerUiOptions)
-);
+const options = { customCssUrl: 'public/swagger-ui.css' };
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  const type = mime.getType(req.url);
+  if (type === 'text/html') {
+    res.setHeader('Content-Type', 'text/css');
+  }
+  next();
+});
+
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
 
 // Export the app
 module.exports = app;
