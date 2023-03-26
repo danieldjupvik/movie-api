41cec94183c0be94c13504905ed7db5378cc7f34
danieldjupvik <daniel@kravia.ai>
2023-03-26 01:04:40 +0100

    use css from cdn

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -2,23 +2,19 @@ const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const path = require('path');
-const mime = require('mime');
 
 const app = express();
 
 const swaggerDocument = YAML.load(path.join(__dirname, './swagger.yaml'));
 
-const options = { customCssUrl: 'public/swagger-ui.css' };
+const options = {
+  swaggerOptions: {
+    customCssUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.18.1/swagger-ui.css',
+  },
+};
 
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use((req, res, next) => {
-  const type = mime.getType(req.url);
-  if (type === 'text/html') {
-    res.setHeader('Content-Type', 'text/css');
-  }
-  next();
-});
 
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
 
